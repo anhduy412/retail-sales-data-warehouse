c4fefb2e64216afd54c6395d1143899a7070181c
Duy Anh Tran <anhduytran5454@gmail.com>
2023-04-24 16:38:46 +0700

    add etl pipeline for fact table.

--- etl-pipeline-fact-table.py
+++ etl-pipeline-fact-table.py
@@ -14,11 +14,26 @@ database = config.database
 conn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server}; SERVER='+server+';DATABASE='+database+';ENCRYPT=yes;Trusted_Connection=yes;')
 cursor = conn.cursor()
 
-#Create Orders table 
+#Create Benefit per Order table 
 cursor.execute('''
-                CREATE TABLE dim.orders(
-                    Market NVARCHAR(50),
-                    Order City NVARCHAR(50),
+                CREATE TABLE fact.benefit(
+                    Shipment Key INT IDENTITY(1,1) FOREIGN KEY REFERENCES dim.shipment(Shipment Key),
+                    Category Key INT IDENTITY(1,1) FOREIGN KEY REFERENCES dim.category(Category Key),
+                    Product Key INT IDENTITY(1,1) FOREIGN KEY REFERENCES dim.product(Product Key),
+                    Customer Key INT IDENTITY(1,1) FOREIGN KEY REFERENCES dim.customer(Customer Key),
+                    Order Key INT IDENTITY(1,1) FOREIGN KEY REFERENCES dim.orders(Order Key),
+                    Order Id INT FOREIGN KEY REFERENCES dim.orders(Order Id),
+                    Department Key INT IDENTITY(1,1) FOREIGN KEY REFERENCES dim.department(Department Key),
+                    Store Key INT IDENTITY(1,1) FOREIGN KEY REFERENCES dim.store(Store Key),
+                    Type NVARCHAR(50),
+                    Order Item Discount FLOAT,
+                    Order Item Discount Rate FLOAT,
+                    Order Item Product Price FLOAT,
+                    Order Item Profit Ratio FLOAT,
+                    Order Item Quantity INT,
+                    Sales FLOAT,
+                    Order Item Total FLOAT,
+                    Order Profit Per Order FLOAT,
                 )'''
 )
 
@@ -27,9 +42,17 @@ for row in df.itertuples():
     cursor.execute('''
                     INSERT INTO RetailSales.dim.orders(
                         
-                    ) VALUES (?,)qweqe
+                    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
                     )''', 
-                    row.market,
+                    row.type,
+                    row.order_item_discount,
+                    row.order_item_discount_rate,
+                    row.order_item_product_price,
+                    row.order_item_profit_ratio,
+                    row.order_item_quantity,
+                    row.sales,
+                    row.order_item_total,
+                    row.order_profit_per_order,
                 )
 conn.commit()
 cursor.close()
