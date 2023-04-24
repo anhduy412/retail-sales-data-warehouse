3f55e1182f5a1bdab278ce0bdf97b162894b8660
Duy Anh Tran <anhduytran5454@gmail.com>
2023-04-24 17:45:09 +0700

    fix connection

--- config.py
+++ config.py
@@ -2,20 +2,23 @@ import pandas as pd
 import pyodbc
 
 #import csv file into dataframe and clean table and coulumn names (lowercase letters. remove spaces and special characters)
-data = pd.read_csv('data/DataCoSupplyChainDataset.csv', encoding = 'latin-1')
-df = pd.DataFrame(data)
-df.columns = [x.lower().replace(' ', '_').replace('(', '').replace(')', '') for x in df.columns]
-print(df.info)
+# data = pd.read_csv('data/DataCoSupplyChainDataset.csv', encoding = 'latin-1')
+# df = pd.DataFrame(data)
+# df.columns = [x.lower().replace(' ', '_').replace('(', '').replace(')', '') for x in df.columns]
+# print(df.info)
 
 # check the pyodbc drivers
-print(pyodbc.drivers())
+# print(pyodbc.drivers())
 
-# #some required variables for SQL connection
+#some required variables for SQL connection
 database = 'RetailSales' #database name
 server = 'ROG' #server name
-conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server}; SERVER='+server+'; DATABASE='+database+'; ENCRYPT=yes; Trusted_Connection=yes;')
+username = 'sa' #username
+password = 'c&NU2v7XDa@ECYHT' #password
+conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server}; SERVER='+server+'; DATABASE='+database+'; ENCRYPT = yes; Trusted_Connection = no; UID='+username+'; PWD='+ password +'')
+
 # Some code to test connection
-# cursor = conn.cursor()
-# cursor.execute("SELECT @@version;")
-# while row := cursor.fetchone():
-#     print(row[0])
+cursor = conn.cursor()
+cursor.execute("SELECT @@version;")
+while row := cursor.fetchone():
+    print(row[0])
