f310743d6ef32e1ef50d573c3a725f396ca07ba5
Duy Anh Tran <anhduytran5454@gmail.com>
2023-04-25 10:05:04 +0700

    fix connection and etl pipeline

--- etl-pipeline-department.py
+++ etl-pipeline-department.py
@@ -7,29 +7,19 @@ df = config.df
 #Connect Python to SQL Server
 server = config.server
 database = config.database
+username = config.username
+password = config.password
 
-conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server}; SERVER='+server+';DATABASE='+database+'; ENCRYPT=yes; Trusted_Connection=yes;')
+conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server}; SERVER='+server+'; DATABASE='+database+'; ENCRYPT = yes; Trusted_Connection = yes; UID='+username+'; PWD='+ password +'')
 cursor = conn.cursor()
 
 #Create Department table 
-cursor.execute('''
-                CREATE TABLE dim.department(
-                    Dept Key INT IDENTITY(1,1) PRIMARY KEY,
-                    Department Id INT,
-                    Department Name NVARCHAR(50),
-                )'''
-)
+cursor.execute("""CREATE TABLE department(department_key INT IDENTITY(1,1) PRIMARY KEY, department_id INT, department_name NVARCHAR(50))""")
 
 # Insert DataFrame to Table
 for row in df.itertuples():
-    cursor.execute('''
-                    INSERT INTO RetailSales.dim.department(
-                        Department Id,
-                        Department Name,
-                    ) VALUES (?,?)
-                    )''', 
-                    row.department_id, 
-                    row.department_name,
+    cursor.execute(
+        f"INSERT INTO [dbo].[department] (department_id, department_name) VALUES ({row.department_id}, '{row.department_name}');"
     )
 conn.commit()
 cursor.close()
