63f1a9dbe4e49c7082acddefecc70f775fd7359b
Duy Anh Tran <anhduytran5454@gmail.com>
2023-04-26 17:46:09 +0700

    fix something

---
+++ date-dimension-table.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import pyodbc
+import config
+
+#Create date dimension dataframe
+date_df = pd.DataFrame(pd.date_range('1/1/2014','12/31/2024'), columns=['day'])
+date_df['year'] = date_df['day'].dt.year
+date_df['quarter_number'] = date_df['day'].dt.quarter
+date_df['quarter_text'] = date_df['day'].apply(lambda x: f'Q{x.quarter} {x.strftime("%Y")}')
+date_df['month'] = date_df['day'].dt.month
+date_df['year_month'] = date_df['day'].dt.strftime("%B %Y")
+date_df['week'] = date_df['day'].dt.isocalendar().week
+date_df['year_week'] = date_df['day'].apply(lambda x: f'{x.isocalendar()[0]} Week {x.isocalendar()[1]}')
+date_df['week_start'] = date_df['day'].dt.to_period('W').apply(lambda x: x.start_time)
+date_df['week_end'] = date_df['day'].dt.to_period('W').apply(lambda x: x.end_time).dt.day
+date_df['weekday'] = date_df['day'].dt.strftime("%A")
+
+date_df = pd.DataFrame(date_df)
+print(date_df)
+
+#Connect Python to SQL Server
+server = config.server
+database = config.database
+username = config.username
+password = config.password
+
+conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server}; SERVER='+server+'; DATABASE='+database+'; ENCRYPT = yes; Trusted_Connection = yes; UID='+username+'; PWD='+ password +'')
+cursor = conn.cursor()
+
+# Create date table
+cursor.execute("""CREATE TABLE date(
+    date_key INT IDENTITY(1,1) PRIMARY KEY,
+    day DATE,
+    year INT,
+    quarter_number INT,
+    quarter_text NVARCHAR(50),
+    month INT,
+    year_month NVARCHAR(50),
+    week INT,
+    year_week NVARCHAR(50),
+    week_start DATE,
+    week_end DATE,
+    weekday NVARCHAR(50)
+    )"""
+)
+
+# Insert DataFrame to Table
+for row in date_df.itertuples():
+    cursor.execute(
+        """INSERT INTO [dbo].[date](
+            day,
+            year,
+            quarter_number,
+            quarter_text,
+            month,
+            year_month,
+            week,
+            year_week,
+            week_start,
+            week_end,
+            weekday
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+        """, 
+        row.day,
+        row.year,
+        row.quarter_number,
+        row.quarter_text,
+        row.month,
+        row.year_month,
+        row.week,
+        row.year_week,
+        row.week_start,
+        row.week_end,
+        row.weekday
+    )
+conn.commit()
+cursor.close()
