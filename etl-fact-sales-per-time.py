f01e6f82d8a0638abec615bd15f2fa1d204a5edd
Duy Anh Tran <anhduytran5454@gmail.com>
2023-05-06 21:17:29 +0700

    fix something

--- etl-fact-sales-per-time.py
+++ etl-fact-sales-per-time.py
@@ -22,12 +22,20 @@ cursor.execute("""CREATE TABLE fact_sales_per_time(
     )"""
 )
 
+#Perform join
+cursor.execute("""SELECT
+    shipment_key, 
+    category_key, 
+    product_key, 
+    customer_key, 
+    order_key, 
+    department_key, 
+    store_key,
+    date_key
+    FROM [dbo].[shipment] s, [dbo].[category] c, [dbo].[product] p, [dbo].[customer] cust, [dbo].[orders] o, [dbo].[department] d, [dbo].[store] st, [dbo].[date] dt
+    LEFT JOIN [dbo].[benefits] b ON b.shipment_key = s.shipment_key, b.category_key = c.category_key, b.product_key = p.product_key, b.customer_key = cust.customer_key, b.order_key = o.order_key, b.department_key = d.department_key, b.store_key = st.store_key, b.date_key = dt.date_key)"""
+)
 
-# Insert DataFrame to Table
-for row in df.itertuples():
-    cursor.execute(
-        f"INSERT INTO [dbo].[fact_sales_per_time] (order_item_quantity, ) VALUES ('{row.order_item_quantity}, '{row.order_date_dateorders}');"
-    )
 
 # Insert DataFrame to Table
 for row in df.itertuples():
