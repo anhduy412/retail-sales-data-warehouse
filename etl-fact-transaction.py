5ca0a1c34fe1301eedfe81de24126b03977bdbe7
Duy Anh Tran <anhduytran5454@gmail.com>
2023-05-09 10:46:29 +0700

    add 1 more fact table and fix somethinng

---
+++ etl-fact-transaction.py
@@ -0,0 +1,34 @@
+import pyodbc
+import config
+
+#Import dataframe
+df = config.df
+
+#Connect Python to SQL Server
+server = config.server
+database = config.database
+username = config.username
+password = config.password
+
+conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server}; SERVER='+server+'; DATABASE='+database+'; ENCRYPT = yes; Trusted_Connection = yes; UID='+username+'; PWD='+ password +'')
+cursor = conn.cursor()
+
+#Create benefits table 
+cursor.execute("""CREATE TABLE fact_sales(
+    
+    )"""
+)
+
+# Perform Join with other tables
+cursor.execute("""SELECT
+               """
+)
+
+# Insert DataFrame to Table
+for row in df.itertuples():
+    cursor.execute(
+        f"INSERT INTO [dbo].[fact_sales] (type, order_item_discount, order_item_discount_rate, order_item_product_price, order_item_profit_ratio, order_item_quantity, sales, order_item_total, order_profit_per_order) WHERE () NOT IN (SELECT * FROM [dbo].[dim_]) VALUES ('{row.type}', {row.order_item_discount}, {row.order_item_discount_rate}, {row.order_item_product_price}, {row.order_item_profit_ratio}, {row.order_item_quantity}, {row.sales}, {row.order_item_total}, {row.order_profit_per_order});"
+    )
+
+conn.commit()
+cursor.close()
--- queries.sql
+++ queries.sql
@@ -11,3 +11,10 @@ SELECT * FROM [dbo].[date];
 SELECT * FROM [dbo].[sales];
 SELECT * FROM [dbo].[sales_per_time];
 SELECT * FROM [dbo].[dim_date];
+
+SELECT fact_table.column1, dim_table1.column2, dim_table2.column3
+FROM fact_table
+INNER JOIN dim_table1
+ON fact_table.dim_table1_id = dim_table1.dim_table1_id
+INNER JOIN dim_table2
+ON fact_table.dim_table2_id = dim_table2.dim_table2_id;
