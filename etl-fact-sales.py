25ffe20f451aee29712820d70e293e5f80e6f2fe
Duy Anh Tran <anhduytran5454@gmail.com>
2023-05-22 04:51:27 +0700

    fix something

--- etl-fact-sales.py
+++ etl-fact-sales.py
@@ -25,17 +25,18 @@ cursor.execute("""
         department_key INT FOREIGN KEY REFERENCES dim_department(department_key),
         discount_key INT FOREIGN KEY REFERENCES dim_discount(discount_key),
         shipment_key INT FOREIGN KEY REFERENCES dim_shipment(shipment_key),
+        profit_key INT FOREIGN KEY REFERENCES dim_profit(profit_key),
         order_item_quantity INT,
         order_item_total FLOAT,
         sales FLOAT,
-        sales_per_customer FLOAT,
+        sales_per_customer FLOAT
     )"""
 )
 conn.commit()
 
 # Insert DataFrame to Table and perform Join with other tables
 cursor.execute("""
-    INSERT INTO fact_sales(order_key, date_key, customer_key, product_key, category_key, store_key, department_key, discount_key, shipment_key, order_item_quantity, order_item_total, sales, sales_per_customer)
+    INSERT INTO fact_sales(order_key, date_key, customer_key, product_key, category_key, store_key, department_key, discount_key, shipment_key, profit_key, order_item_quantity, order_item_total, sales, sales_per_customer)
     SELECT
         dim_orders.order_key,
         dim_date.date_key,
@@ -46,10 +47,11 @@ cursor.execute("""
         dim_department.department_key,
         dim_discount.discount_key,
         dim_shipment.shipment_key,
-        order_item_quantity, 
-        order_item_total,
-        sales,
-        sales_per_customer
+        dim_profit.profit_key,
+        dcscd.order_item_quantity,
+        dcscd.order_item_total,
+        dcscd.sales,
+        dcscd.sales_per_customer
     FROM dcscd
     JOIN dim_orders ON dcscd.order_id = dim_orders.order_id
     JOIN dim_date ON dcscd.order_date_dateorders = dim_date.day
@@ -60,6 +62,7 @@ cursor.execute("""
     JOIN dim_department ON dcscd.department_id = dim_department.department_id
     JOIN dim_discount ON dcscd.order_item_discount = dim_discount.order_item_discount AND dcscd.order_item_discount_rate = dim_discount.order_item_discount_rate
     JOIN dim_shipment ON dcscd.days_for_shipment_scheduled = dim_shipment.days_for_shipment_scheduled AND dcscd.days_for_shipping_real = dim_shipment.days_for_shipping_real AND dcscd.shipping_date_dateorders = dim_shipment.shipping_date_dateorders AND dcscd.shipping_mode = dim_shipment.shipping_mode
+    JOIN dim_profit ON dcscd.order_item_product_price = dim_profit.order_item_product_price AND dcscd.order_item_profit_ratio = dim_profit.order_item_profit_ratio AND dcscd.order_item_quantity = dim_profit.order_item_quantity AND dcscd.order_item_total = dim_profit.order_item_total AND dcscd.order_profit_per_order = dim_profit.order_profit_per_order AND dcscd.sales_per_customer = dim_profit.sales_per_customer
     """
 )
 conn.commit()
