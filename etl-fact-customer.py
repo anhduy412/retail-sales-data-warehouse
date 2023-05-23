fa05fed9ddf8213d17ea19a19feab3380d504047
Duy Anh Tran <anhduytran5454@gmail.com>
2023-05-23 10:29:26 +0700

    fix the fact customer table

--- etl-fact-customer.py
+++ etl-fact-customer.py
@@ -19,10 +19,10 @@ cursor.execute("""
     CREATE TABLE fact_customer(
         customer_key INT FOREIGN KEY REFERENCES dim_customer(customer_key),
         date_key INT FOREIGN KEY REFERENCES dim_date(date_key),
+        store_key INT FOREIGN KEY REFERENCES dim_store(store_key),
         order_key INT FOREIGN KEY REFERENCES dim_orders(order_key),
-        product_key INT FOREIGN KEY REFERENCES dim_product(product_key),
         discount_key INT FOREIGN KEY REFERENCES dim_discount(discount_key),
-        type NVARCHAR(50),
+        sales FLOAT,
         order_item_quantity INT,
         order_item_total FLOAT
     )"""
@@ -31,21 +31,21 @@ conn.commit()
 
 # Insert DataFrame to Table and perform Join with other tables
 cursor.execute("""
-    INSERT INTO fact_customer(customer_key, date_key, order_key, product_key, discount_key, type, order_item_quantity, order_item_total)
+    INSERT INTO fact_customer(customer_key, date_key, store_key, order_key, discount_key, sales, order_item_quantity, order_item_total)
     SELECT
         dim_customer.customer_key,
         dim_date.date_key,
+        dim_store.store_key,
         dim_orders.order_key,
-        dim_product.product_key,
         dim_discount.discount_key,
-        type,
+        sales,
         order_item_quantity,
         order_item_total
     FROM dcscd
     JOIN dim_customer ON dcscd.customer_id = dim_customer.customer_id
     JOIN dim_orders ON dcscd.order_id = dim_orders.order_id
+    JOIN dim_store ON dcscd.latitude = dim_store.latitude AND dcscd.longitude = dim_store.longitude
     JOIN dim_date ON dcscd.order_date_dateorders = dim_date.day
-    JOIN dim_product ON dcscd.product_card_id = dim_product.product_card_id
     JOIN dim_discount ON dcscd.order_item_discount = dim_discount.order_item_discount AND dcscd.order_item_discount_rate = dim_discount.order_item_discount_rate
     """
 )
